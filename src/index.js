18c3703e7b3be541c77c4fe4062e3bc784dee19c
Umme Salma <salmaherdoy@gmail.com>
2023-01-13 13:07:48 +0600

    Removed Font-Awesome

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import "font-awesome/css/font-awesome.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
